cab364e45bc637c693d2bd36270d607a2a9afadc
Phan Thanh Danh <phanthanhdanh657@gmail.com>
2026-03-29 11:45:58 +0700

    Add file and change somethings

--- scr/bin/CLI/_CLI_.py
+++ scr/bin/CLI/_CLI_.py
@@ -1,27 +1,52 @@
 # CLI - @CodeHaveFun
+# ALL Commands CLI for Core Version 0.0.1
+#(MODE1): core
+# --> 1. 'build'
+#(MODE2): help
+# --> 1. '-cmd'
+
+
 
 # parse token input
-import shlex as core_parse 
+import shlex as coreCLI_parse
+import os as coreSys
 import _CLI_TEXT_ as CTE
-
+# Run 
 def _Excus_CMD_(_IN_TOKEN_):
-    if(_IN_TOKEN_[0] == "core"):   # Token Main 
-        if(_IN_TOKEN_[1] == "run"):
-            print("run: ok")
+    # check cmd have mode cmd?
+    if not(len(_IN_TOKEN_) > 0):
+        pass
+    else:    
+        if(_IN_TOKEN_[0] == "core"):   # Token Main
+            # Check
+            if not (len(_IN_TOKEN_) > 1):
+                print(f"Core$: {CTE.WRG_MSS_CMD_TEXT} for 'core' command \nCore$: Err: {CTE.WRG_MSS_CMD_ERR}\n")
+            else:
+                # Core: Build File
+                if(_IN_TOKEN_[1] == "build"):
+                    # Check location
+                    if not(len(_IN_TOKEN_) > 2):
+                        print(f"Core$ Build: {CTE.WRG_BLD_NED_LCT_TEXT} \nCore$ Build Err: {CTE.WRG_BLD_NED_LCT_ERR}\n")
+                    else:
+                        # Check right path and file
+                        if (coreSys.path.exists(_IN_TOKEN_[2]) and coreSys.path.isfile(_IN_TOKEN_[2])):
+                            print("okk")
+                        else:
+                            print(f"Core$ Build Locat: {CTE.WRG_BLD_CNT_SEF_TEXT} \nCore$ Build Locat: {CTE.WRG_BLD_CNT_SEF_ERR}\n")
+                else:
+                    print(f"Core$: '{_IN_TOKEN_[1]}' {CTE.WRG_CMD_1_TEXT} \nCore$: {CTE.WRG_CMD_1_ERR}\n")
         else:
-            print(f"Core$: '{_IN_TOKEN_[1]}' {CTE.WRG_CMD_1_TEXT}")
-            print(f"Core$: {CTE.WRG_CMD_1_ERR} \n")
-            
+            # Case 1: Error Typing
+            print(f"'{_IN_TOKEN_[0]}' {CTE.WRG_CMD_1_TEXT} \nErr: {CTE.WRG_CMD_1_ERR}\n") 
 
 
 def _CoreCLI_():
     while True:
-        Core_Args = core_parse.split(input("~$ "))
-        if Core_Args[0] == "core" or Core_Args[0] == "-help":
+        try:
+            Core_Args = coreCLI_parse.split(input("~$ "), posix=False)
             _Excus_CMD_(Core_Args)
-        else:
-            # Case 1: Error Typing
-            print(f"'{Core_Args[0]}' {CTE.WRG_CMD_1_TEXT}") 
-            print(f"Err: {CTE.WRG_CMD_1_ERR} \n")
+        except Exception:
+            pass
+            print()
 if __name__ == "__main__":
     _CoreCLI_()
